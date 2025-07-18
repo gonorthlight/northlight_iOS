21003fb374c58a7dc6a9d3d604861d1697ab2bff
Daniel Kilders <danielkildersdiaz@gmail.com>
2025-07-18 18:30:21 +0200

    Update Package.swift for version 0.7.8

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NorthlightSDK",
-            url: "https://github.com/gonorthlight/northlight_iOS/releases/download/0.7/NorthlightSDK.xcframework.zip",
-            checksum: "b179e93a5343d489d1841d07ee9bd90eb32839a9c699e2c4ae4b9f84f8e9e558"
+            url: "https://github.com/gonorthlight/northlight_iOS/releases/download/0.7.8/NorthlightSDK.xcframework.zip",
+            checksum: "9352a662f641533bb4ab425b0c8b64ac563a771d4ec72865403a5f955202ffe2"
         )
     ]
 )
